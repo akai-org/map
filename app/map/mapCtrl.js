3eba6a647b2a1ae2f8b27caede3d1e5eb02a1923
Rafał Rudol <rrrrudol@gmail.com>
2017-01-03 23:30:00 +0100

    add leaflet

--- app/map/mapCtrl.js
+++ app/map/mapCtrl.js
@@ -5,7 +5,13 @@ define([], function() {
 
     function MapCtrl($scope) {
       this.$scope = $scope;
-      
+      var mymap = L.map('mapid').setView([52.40257, 16.94933], 17);
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 18
+      }).addTo(mymap);
+      var marker = L.marker([52.40407, 16.94968]).addTo(mymap);
+      marker.bindPopup("<b>Centrum Wykładowe</b><br>Hello World. <a href='#'>Wejdź do środka</a>").openPopup();
     }
 
     // tutaj funkcje
