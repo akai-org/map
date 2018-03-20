457009b6951bde7b5f5d1c50a95b22cfb609a077
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-03-20 18:19:46 +0100

    add buildings

--- app/buildings/buildingsCtrl.js
+++ app/buildings/buildingsCtrl.js
@@ -8,17 +8,39 @@ define([], function() {
       this.$state = $state;
       this.$stateParams = $stateParams;
 
-      this.$scope.test = "building id: " + this.$stateParams.id;
-
       this.initializeMap();
     }
 
     BuildingsCtrl.prototype.initializeMap = function () {
       this.$scope.center = {
-        lat: 52.40257,
-        lng: 16.94933,
-        zoom: 17
+        lat: 0,
+        lng: 0,
+        zoom: 1
       };
+      this.$scope.layers = {
+        baselayers: {
+            andes: {
+                name: 'Andes',
+                type: 'imageOverlay',
+                url: 'resources/svg/CW1.svg',
+                bounds: [[-540, -960], [540, 960]],
+                layerParams: {
+                  noWrap: true,
+                  attribution: 'Creative Commons image found <a href="http://www.flickr.com/photos/c32/8025422440/">here</a>'
+                }
+            }
+        },
+      }
+
+      this.$scope.markers = {
+        osloMarker: {
+            lat: 50,
+            lng: 50,
+            message: "I want to travel here!",
+            focus: true,
+            draggable: false
+        }
+      }
     };
 
     return BuildingsCtrl;
