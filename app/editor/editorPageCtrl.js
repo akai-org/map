b4bbc784cf5ee8d05040ffb392067a6fdeebb3b4
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-03-27 17:53:44 +0200

    add editor for buildings-data.json

--- app/editor/editorPageCtrl.js
+++ app/editor/editorPageCtrl.js
@@ -7,6 +7,9 @@ define([], function () {
       this.$scope = $scope;
       this.buildingUtil = buildingUtil;
       this.getBuildingsData();
+
+      this.$scope.addFloor = angular.bind(this, this.addFloor);
+      this.$scope.saveJsonFile = angular.bind(this, this.saveJsonFile);
     }
 
     EditorPageCtrl.prototype.getBuildingsData = function() {
@@ -23,6 +26,54 @@ define([], function () {
     EditorPageCtrl.prototype.getBuildingsDataErrorHandler = function(error) {
       console.log(error);
     };
+
+    EditorPageCtrl.prototype.addFloor = function(buildingProperties) {
+      var floors = buildingProperties.floors ? buildingProperties.floors : [];
+      var floor = {};
+      if (floors && floors.length > 0) {
+        var maxId = floors.map(function(f) {
+          return f.id;
+        }).reduce(function(acc, next) {
+          if (next > acc) {
+            acc = next;
+          }
+          return acc;
+        }, 0)
+        
+        floor = {
+          id: ++maxId,
+          name: '',
+          pictureURL: ''
+        }
+      } else {
+        floor = {
+          id: 1,
+          name: '',
+          pictureURL: ''
+        }
+      }
+      floors.push(floor);
+      buildingProperties.floors = floors;
+    }
+
+    EditorPageCtrl.prototype.saveJsonFile = function() {
+      var successHandler = angular.bind(this, this.saveJsonFileSuccessHandler);
+      var errorHandler = angular.bind(this, this.saveJsonFileErrorHandler);
+      this.buildingUtil.getBuildingsData().then(successHandler, errorHandler);
+    }
+
+    EditorPageCtrl.prototype.saveJsonFileSuccessHandler = function(response) {
+      var buildingsDataJson = response.data;
+      buildingsDataJson.features = this.$scope.buildingsData;
+      var a = document.createElement('a');
+      a.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(JSON.stringify(buildingsDataJson)));
+      a.setAttribute('download', 'buildings-data.json');
+      a.click();
+    }
+
+    EditorPageCtrl.prototype.saveJsonFileErrorHandler = function(error) {
+      console.log(error);
+    }
 
     return EditorPageCtrl;
   })();
