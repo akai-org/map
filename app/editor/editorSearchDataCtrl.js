2181a9e4a9bcb46596c24331b7447c1611366546
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-03-28 18:49:52 +0200

    add editorSearchData

--- app/editor/editorSearchDataCtrl.js
+++ app/editor/editorSearchDataCtrl.js
@@ -6,6 +6,10 @@ define([], function () {
       function EditorSearchDataCtrl($scope, searchDataUtil) {
         this.$scope = $scope;
 				this.searchDataUtil = searchDataUtil;
+
+				this.$scope.addRoom = angular.bind(this, this.addRoom);
+				this.$scope.saveJsonFile = angular.bind(this, this.saveJsonFile);
+
 				this.getBaseSearchData();
       }
 
@@ -17,13 +21,50 @@ define([], function () {
 			
 			EditorSearchDataCtrl.prototype.getBaseSearchDataSuccessHandler = function(response) {
 				this.$scope.searchData = response.data;
-				console.log(response);
+				this.$scope.buildings = this.searchDataUtil.convertBaseSearchDataToFormData(this.$scope.searchData);
 			};
 
 			EditorSearchDataCtrl.prototype.getBaseSearchDataErrorHandler = function(error) {
 				console.log(error);
 			};
-  
+
+			EditorSearchDataCtrl.prototype.addRoom = function(building) {
+				var maxRoomId = this.getMaxRoomId();
+				var room = {
+					roomId: ++maxRoomId,
+					campus: building.campus,
+					buildingId: building.buildingId,
+					name: '',
+					aliasesString: '',
+					aliases: []
+				};
+				building.rooms.push(room);
+			};
+
+			EditorSearchDataCtrl.prototype.getMaxBuildingId = function() {
+				var buildingIds = this.$scope.buildings.map(function(b) {
+					return b.buildingId;
+				});
+				return Math.max.apply(null, buildingIds);
+			};
+
+			EditorSearchDataCtrl.prototype.getMaxRoomId = function() {
+				var rooms = [];
+				for (var i=0; i<this.$scope.buildings.length; i++) {
+					rooms = rooms.concat(this.$scope.buildings[i].rooms);
+				};
+				var roomIds = rooms.map(function(b) {
+					return b.roomId;
+				});
+				return Math.max.apply(null, roomIds);
+			};
+
+			EditorSearchDataCtrl.prototype.saveJsonFile = function() {
+				var a = document.createElement('a');
+				a.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(JSON.stringify(this.$scope.buildings)));
+				a.setAttribute('download', 'base-search-data.json');
+				a.click();
+			};
   
       return EditorSearchDataCtrl;
     })();
--- app/services/searchDataUtil.js
+++ app/services/searchDataUtil.js
@@ -16,11 +16,35 @@ define([], function() {
     };
 
     SearchDataUtil.prototype.convertBaseSearchDataToFormData = function(baseSearchData) {
-
+      var data = this.convertAliasesToString(baseSearchData);
+      var buildings = data.filter(function(d) {
+        return d.roomId == null || d.roomId == undefined;
+      });
+      this.prepareRooms(buildings, data);
+      console.log(buildings);
+      return buildings;
     };
 
     SearchDataUtil.prototype.convertFormDataToBaseSearchData = function(formData) {
       
+    };
+
+    SearchDataUtil.prototype.prepareRooms = function(buildings, baseSearchData) {
+      for (var i=0; i<buildings.length; i++) {
+        var building = buildings[i];
+        building.rooms = [];
+        var rooms = baseSearchData.filter(function(r) {
+          return r.roomId && r.buildingId === building.buildingId;
+        });
+        building.rooms = rooms;
+      }
+    };
+
+    SearchDataUtil.prototype.convertAliasesToString = function(baseSearchData) {
+      return baseSearchData.map(function(b) {
+        b.aliasesString = b.aliases.join(', ');
+        return b;
+      });
     };
 
     return SearchDataUtil;
